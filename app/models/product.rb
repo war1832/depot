a804b663524214ef9fdb8e404ca66f538f37dda6
War <war.v8sf@gmail.com>
2015-03-09 20:39:26 -0300

    Check Out

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
   has_many :line_items
+  has_many :orders, through: :line_items
+  #...
 
   before_destroy :ensure_not_referenced_by_any_line_item
-
-  #...
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -18,6 +18,7 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+
   private
 
     # ensure that there are no line items referencing this product
